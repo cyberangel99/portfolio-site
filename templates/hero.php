8f64fe3f5ee1e5bc5c9c48a9aa9414643b81145e
Mary Green <cyberangel9911@gmail.com>
2017-09-01 22:01:25 -0500

    hero image
    added hero image and button

---
+++ templates/hero.php
@@ -0,0 +1,7 @@
+<div class="uk-cover-container" id="hero" uk-height-viewport>
+    <div class="uk-container uk-padding uk-text-center">
+        <h1>Mary A. Green</h1>
+        <h2>Junior Web Developer and Gamer Nerd</h2>
+        <button class="uk-button uk-button-primary uk-button-large"><i class="fa fa-laptop"></i>  Portfolio</button>
+    </div>
+</div>
