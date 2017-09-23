338b806c1b5424a205bcd61e49c66c12b2576d78
Mary Green <cyberangel9911@gmail.com>
2017-09-22 22:21:36 -0500

    about section
    updated about section and hero icons

--- templates/about.php
+++ templates/about.php
@@ -1,6 +1,9 @@
 <section id="about" class="uk-section uk-padding-remove-top uk-padding-remove-bottom">
     <div uk-grid class="uk-text-center">
-        <div class="uk-width-1-2@m uk-padding me"><h2>Who am I?</h2><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse sit amet laoreet tortor, et porttitor tortor. Sed gravida lobortis hendrerit. Ut in aliquam urna. Nunc dapibus auctor libero, eu aliquet augue malesuada id. Cras vel orci ex. Ut non augue elit. Phasellus consectetur velit dolor, vitae posuere nibh aliquet at. Aliquam erat volutpat. Ut condimentum, augue vitae vehicula pulvinar, ligula quam euismod nunc, sit amet venenatis sapien nulla quis odio.</p></div>
+        <div class="uk-width-1-2@m uk-padding me"><h2 class="uk-margin-remove">Who am I?</h2><p class="uk-padding-small uk-margin-remove font">I am a junior web developer and I have basic skills in HTML, CSS, SASS/LESS, Javascript, and JQuery. I have been self-teaching myself these skills along with studying in class. I am very ambitious to learn more basic skills and progress to advance coding skills. My goal is to work as a Front-End web developer and my long-term goal is to become a back-end developer.</p> 
+                                                <p class="uk-margin-remove uk-padding-small font">I am a cheerful laid-back gal who is originally from Northwest Ohio. I currently live in the Chicago Suburbs and I plan to move to Houston, TX next year. I am college student and my major is web development.</p> 
+                                                <p class="uk-margin-remove uk-padding-small font">Overall I'm just a geeky girl, who loves travelling, coding, gaming, watching anime, and riding my bike. I have a nice collection of games and collectables that I love to collect and play!</p>
+        </div>
         <div class="uk-width-1-2@m uk-padding-remove-left">
             <div class="uk-overflow-hidden">
                 <img class="uk-animation-kenburns" src="./assets/images/aboutme.jpeg" alt="aboutme image"/>
