a61372cfc13fe6fa5915812aa622a22870e782ca
Mary Green <cyberangel9911@gmail.com>
2017-10-23 20:39:34 -0500

    updated sections
    updated text in sections and added social links

--- templates/contact.php
+++ templates/contact.php
@@ -1,8 +1,8 @@
 <section id="contact" class="uk-padding-remove-top">
     <div uk-grid>
         <div class="uk-width-1-2@m uk-padding uk-background-muted"><img src="./assets/images/contactme.png" alt="contact me"/></div>
-        <div class="uk-width-1-2@m uk-padding uk-text-center"><h2>Contact me!</h2><p class="uk-padding-small">Interested in my portfolio? Email me <a href="marygreen980@gmail.com" class="email-link">here</a>. I will be glad to respond.
-<p>Don’t want to email? Call me or leave me a message at 419-789-1992, I will respond as soon as I receive your message.</p>
+        <div class="uk-width-1-2@m uk-padding uk-text-center"><h2>Contact me!</h2><p class="uk-padding-small">Interested in my portfolio? Email me <span class="email-link"><a href="mailto:magreen451@gmail.com">here</span></a>. I will be glad to respond.
+<p>Don’t want to email? Call me or leave me a message at 419-789-1992, I will respond as soon as I receive your message. Don't want to do either? Lets connect thru my <span class="linkin"><a href="https://www.linkedin.com/in/mary-green-4295ab149/">LinkedIn!</span></a></p>
 
 </p></div>
         </p></div>
