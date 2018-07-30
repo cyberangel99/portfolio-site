9e5e41ce2099dfd4e66fa7d40adcfe113910b31d
Mary Green <cyberangel9911@gmail.com>
2018-07-29 22:03:12 -0500

    contact
    added form to contact section

--- templates/contact.php
+++ templates/contact.php
@@ -1,11 +1,16 @@
 <section id="contact" class="uk-padding-remove-top">
-    <div uk-grid>
+    <div uk-grid class="uk-text-center">
         <div class="uk-width-1-2@m uk-padding uk-background-muted" uk-scrollspy="cls: uk-animation-slide-bottom-medium; repeat: true; delay:500"><img src="./assets/images/contactme.png" alt="contact me"/></div>
-        <div class="uk-width-1-2@m uk-padding uk-text-center" uk-scrollspy="cls: uk-animation-slide-top-medium; repeat: true; delay:500"><h2>Contact me!</h2><p class="uk-padding-small">Interested in my portfolio? Email me <span class="email-link"><a href="mailto:magreen451@gmail.com">here</span></a>. I will be glad to respond.
-<p>Don’t want to email? Call me or leave me a message at 419-789-1992, I will respond as soon as I receive your message. Don't want to do either? Lets connect thru my <span class="linkin"><a href="https://www.linkedin.com/in/mary-green-4295ab149/">LinkedIn!</span></a></p>
-
-</p></div>
-        </p></div>
+                <div class="uk-width-1-2@m uk-padding" uk-scrollspy="cls: uk-animation-slide-top-medium; repeat: true; delay:500">
+                    <h2 class="uk-margin-remove">Contact me</h2>
+                    <form method="post" class="contact" id="contactForm">
+                        <div class="uk-padding-small"><input class="uk-input uk-form-width-medium" type="text" placeholder="First Name"></div>
+                        <div class="uk-padding-small"><input class="uk-input uk-form-width-medium" type="text" placeholder="Last Name"></div>
+                        <div class="uk-padding-small"><input class="uk-input uk-form-width-medium" type="text" placeholder="Phone Number"></div>
+                        <div class="uk-padding-small"><textarea class="uk-textarea uk-width-1-2@s" rows="7" placeholder="Message"></textarea></div>
+                    </form>
+                </div>
+        
     </div>
 
 </section>
