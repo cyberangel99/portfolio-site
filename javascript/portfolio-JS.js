806bb77b8a9f1e4035a06fb48750113e2add456a
Mary Green <cyberangel9911@gmail.com>
2018-07-31 16:09:48 -0500

    contact section
    added submit button

---
+++ javascript/portfolio-JS.js
@@ -0,0 +1,30 @@
+
+$(document).ready(function(){
+    $('contactForm').submit(function(event){
+        //validate array name goes here
+        event.preventdefault();
+    })
+})
+
+
+function checkForErrors(){
+    
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- templates/contact.php
+++ templates/contact.php
@@ -4,11 +4,15 @@
                 <div class="uk-width-1-2@m uk-padding" uk-scrollspy="cls: uk-animation-slide-top-medium; repeat: true; delay:500">
                     <div class="uk-text-center"><h2 class="uk-margin-remove">Contact me</h2></div>
                     <form method="post" class="Form" id="contactForm">
-                        <div class="uk-padding-small"><input class="uk-input uk-form-width-medium" type="text" placeholder="First Name"></div>
-                        <div class="uk-padding-small"><input class="uk-input uk-form-width-medium" type="text" placeholder="Last Name"></div>
-                        <div class="uk-padding-small"><input class="uk-input uk-form-width-medium" type="text" placeholder="Phone Number"></div>
-                        <div class="uk-padding-small"><textarea class="uk-textarea uk-width-1-1@s .uk-form-width-large" rows="7" placeholder="Message"></textarea></div>
+                        <div class="uk-padding-small"><input class="uk-input uk-form-width-medium" name="firstname" type="text" placeholder="First Name"></div>
+                        <div class="uk-padding-small"><input class="uk-input uk-form-width-medium" name="lastname" type="text" placeholder="Last Name"></div>
+                        <div class="uk-padding-small"><input class="uk-input uk-form-width-medium" name="phonenumber" type="text" placeholder="Phone Number"></div>
+                        <div class="uk-padding-small"><textarea class="uk-textarea uk-width-1-1@s .uk-form-width-large" rows="7" name="message" placeholder="Message"></textarea></div>
+                        <div><button class="uk-button uk-button-primary uk-button-small uk-margin-small-left">Submit</button></div>
                     </form>
+
+                    <script type="text/javascript" src="./javascript/jquery-3.3.1.min.js"></script>
+                    <script src="./javascript/portfolio-JS.js"></script>
                 </div>
         
     </div>
