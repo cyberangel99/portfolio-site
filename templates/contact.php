18b2b281a5f198698a0617f71c8573c0b3937f5f
Mary Green <cyberangel9911@gmail.com>
2018-07-30 23:09:58 -0500

    contact section
    aligned the contact form on the contact section

--- templates/contact.php
+++ templates/contact.php
@@ -1,13 +1,13 @@
 <section id="contact" class="uk-padding-remove-top">
-    <div uk-grid class="uk-text-center">
+    <div uk-grid>
         <div class="uk-width-1-2@m uk-padding uk-background-muted" uk-scrollspy="cls: uk-animation-slide-bottom-medium; repeat: true; delay:500"><img src="./assets/images/contactme.png" alt="contact me"/></div>
                 <div class="uk-width-1-2@m uk-padding" uk-scrollspy="cls: uk-animation-slide-top-medium; repeat: true; delay:500">
-                    <h2 class="uk-margin-remove">Contact me</h2>
-                    <form method="post" class="contact" id="contactForm">
+                    <div class="uk-text-center"><h2 class="uk-margin-remove">Contact me</h2></div>
+                    <form method="post" class="Form" id="contactForm">
                         <div class="uk-padding-small"><input class="uk-input uk-form-width-medium" type="text" placeholder="First Name"></div>
                         <div class="uk-padding-small"><input class="uk-input uk-form-width-medium" type="text" placeholder="Last Name"></div>
                         <div class="uk-padding-small"><input class="uk-input uk-form-width-medium" type="text" placeholder="Phone Number"></div>
-                        <div class="uk-padding-small"><textarea class="uk-textarea uk-width-1-2@s" rows="7" placeholder="Message"></textarea></div>
+                        <div class="uk-padding-small"><textarea class="uk-textarea uk-width-1-1@s .uk-form-width-large" rows="7" placeholder="Message"></textarea></div>
                     </form>
                 </div>
         
