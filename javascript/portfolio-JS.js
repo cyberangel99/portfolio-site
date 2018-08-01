e3fc3291d9ddac8f4344e25089804c9736af8133
Mary Green <cyberangel9911@gmail.com>
2018-07-31 23:15:44 -0500

    update
    updated contact section
    working on viewport height

--- javascript/portfolio-JS.js
+++ javascript/portfolio-JS.js
@@ -1,16 +1,56 @@
 
 $(document).ready(function(){
-    $('contactForm').submit(function(event){
-        //validate array name goes here
-        event.preventdefault();
+    $('#contactForm').submit(function(event){
+        checkForErrors()
+        event.preventDefault();
     })
 })
 
 
 function checkForErrors(){
-    
+    var errors= []
+    var fname= $('#fname');
+    var lname= $('#lname');
+    var pnumber=$('#pnumber');
+    var email=$('#email');
+    var message=$('#message');
 
-
+    if(fname.val() == ""){
+        errors.push("Enter your first name");
+        fname.addClass('error')
+    }else{
+        fname.removeClass('error');
+    }
+    if(lname.val() == ""){
+        errors.push("Enter your last name");
+        lname.addClass('error')
+    }else{
+        lname.removeClass('error');
+    }
+    if(pnumber.val() == ""){
+        errors.push("Enter your phone number");
+        pnumber.addClass('error')
+    }else{
+        pnumber.removeClass('error');
+    }
+    if(email.val() == ""){
+        errors.push("Enter your email");
+        email.addClass('error')
+    }else{
+        email.removeClass('error');
+    }
+    if(message.val() == ""){
+        errors.push("Enter your message");
+        message.addClass('error')
+    }else{
+        message.removeClass('error');
+    }
+    if(errors.length > 0){
+        $("#errors").html("");
+        $.each(errors, function(index, errormessages){
+            $('#errors').hide().append(`<p>${errormessages}</p>`).fadeIn(300);
+        })
+    }
 
 }
 
--- templates/contact.php
+++ templates/contact.php
@@ -3,12 +3,15 @@
         <div class="uk-width-1-2@m uk-padding uk-background-muted" uk-scrollspy="cls: uk-animation-slide-bottom-medium; repeat: true; delay:500"><img src="./assets/images/contactme.png" alt="contact me"/></div>
                 <div class="uk-width-1-2@m uk-padding" uk-scrollspy="cls: uk-animation-slide-top-medium; repeat: true; delay:500">
                     <div class="uk-text-center"><h2 class="uk-margin-remove">Contact me</h2></div>
-                    <form method="post" class="Form" id="contactForm">
-                        <div class="uk-padding-small"><input class="uk-input uk-form-width-medium" name="firstname" type="text" placeholder="First Name"></div>
-                        <div class="uk-padding-small"><input class="uk-input uk-form-width-medium" name="lastname" type="text" placeholder="Last Name"></div>
-                        <div class="uk-padding-small"><input class="uk-input uk-form-width-medium" name="phonenumber" type="text" placeholder="Phone Number"></div>
-                        <div class="uk-padding-small"><textarea class="uk-textarea uk-width-1-1@s .uk-form-width-large" rows="7" name="message" placeholder="Message"></textarea></div>
-                        <div><button class="uk-button uk-button-primary uk-button-small uk-margin-small-left">Submit</button></div>
+                    <div id="errors"></div>
+                    <div id="success"></div>
+                    <form class="Form" id="contactForm" method="post">
+                        <div class="uk-padding-small"><input class="uk-input uk-width-1-2" id="fname" name="firstname" type="text" placeholder="First Name"></div>
+                        <div class="uk-padding-small"><input class="uk-input uk-width-1-2" id="lname" name="lastname" type="text" placeholder="Last Name"></div>
+                        <div class="uk-padding-small"><input class="uk-input uk-width-1-2" id="pnumber" name="phonenumber" type="text" placeholder="Phone Number"></div>
+                        <div class="uk-padding-small"><input class="uk-input uk-width-1-2" id="email" name="email" type="text" placeholder="email"></div>
+                        <div class="uk-padding-small"><textarea class="uk-textarea uk-width-1-1@s .uk-form-width-large" rows="7" id="message" name="message" placeholder="Message"></textarea></div>
+                        <div><button class="uk-button uk-button-primary uk-button-small uk-margin-small-left" type="submit">Submit</button></div>
                     </form>
 
                     <script type="text/javascript" src="./javascript/jquery-3.3.1.min.js"></script>
