9250a2b6fea1faa494479e4af6038be4b3149eef
Mary Green <cyberangel9911@gmail.com>
2019-02-03 21:51:15 -0600

    updated contact form
    valiated form on the front and back end

--- public/assets/js/common.js
+++ public/assets/js/common.js
@@ -1,4 +1,4 @@
-$(document).ready(function(){
+$(document).ready(function () {
     /* Smooth scrolling */
     var options = {
         offset: 56,
@@ -6,4 +6,79 @@ $(document).ready(function(){
     };
     UIkit.scroll($(".transformed a"), options);
     UIkit.scroll($("#offcanvas .links a"), options);
+
+    //Contact form validation
+    $('#contactForm').submit(function (event) {
+        event.preventDefault();
+        const formData = $(this).serialize()
+        const errors = checkForErrors()
+        if (errors.length === 0) {
+            //AJAX call
+            postContactData($('#contactForm'), formData);
+        } else {
+            $("#success").hide();
+            $("#errors").html("");
+            $.each(errors, function (index, errormessages) {
+                $('#errors').hide().append(`<p>${errormessages}</p>`).fadeIn(300);
+            })
+        }
+    })
 });
+
+function postContactData(form, formData) {
+    $.post('/contact', formData, function(data) {
+        if(data.errors) {
+            $("#success").hide();
+            $("#errors").html("");
+            $.each(data.errors, function (index, errormessages) {
+                $('#errors').hide().append(`<p>${errormessages.message}</p>`).fadeIn(300);
+            }) 
+        } else {
+            $("#errors").hide();
+            $("#success").hide().html(data.success).slideDown(400);
+            form.trigger("reset");
+        }
+    }, 'json')
+}
+
+function checkForErrors() {
+    var errors = []
+    var fname = $('#fname');
+    var lname = $('#lname');
+    var pnumber = $('#pnumber');
+    var email = $('#email');
+    var message = $('#message');
+
+    if (fname.val() == "") {
+        errors.push("Enter your first name");
+        fname.addClass('error')
+    } else {
+        fname.removeClass('error');
+    }
+    if (lname.val() == "") {
+        errors.push("Enter your last name");
+        lname.addClass('error')
+    } else {
+        lname.removeClass('error');
+    }
+    if (pnumber.val() == "") {
+        errors.push("Enter your phone number");
+        pnumber.addClass('error')
+    } else {
+        pnumber.removeClass('error');
+    }
+    if (email.val() == "") {
+        errors.push("Enter your email");
+        email.addClass('error')
+    } else {
+        email.removeClass('error');
+    }
+    if (message.val() == "") {
+        errors.push("Enter your message");
+        message.addClass('error')
+    } else {
+        message.removeClass('error');
+    }
+
+    return errors
+}
--- src/routes.php
+++ src/routes.php
@@ -12,3 +12,36 @@ $app->get('/', function (Request $request, Response $response, array $args) {
     // Render index view
     return $this->renderer->render($response, 'index.php', $args);
 });
+
+$app->post('/contact', function (Request $request, Response $response, array $args) {
+    $parsedBody = $request->getParsedBody();
+    $firstname = $parsedBody['firstname'];
+    $lastname = $parsedBody['lastname'];
+    $phonenumber = $parsedBody['phonenumber'];
+    $email = $parsedBody['email'];
+    $message = $parsedBody['message'];
+    $errors = array();
+
+    if(empty($firstname)){
+        array_push($errors, array('message' => 'First Name is empty'));
+    }
+    if(empty($lastname)){
+        array_push($errors, array('message' => 'Last Name is empty'));
+    }
+    if(empty($phonenumber)){
+        array_push($errors, array('message' => 'Phone Number is empty'));
+    }
+    if(empty($email) || !filter_var($email, FILTER_VALIDATE_EMAIL)){
+        array_push($errors, array('message' => 'Email is empty or invalid'));
+    }
+    if(empty($message)){
+        array_push($errors, array('message' => 'Message is empty'));
+    }
+
+    if(!empty($errors)){
+        return $response->withJson(array('errors' => $errors));
+    }else{
+        //Send email here...
+        return $response->withJson(array('success' => "Message successfully sent. Thank you {$firstname}!"));
+    }
+});
