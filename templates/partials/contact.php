c94e822f8177a59138c233d62113ba5ab2afc2fc
Mary Green <cyberangel9911@gmail.com>
2019-02-03 22:00:40 -0600

    updated contact form
    added text to identify textboxes

--- templates/partials/contact.php
+++ templates/partials/contact.php
@@ -6,11 +6,11 @@
                     <div id="errors"></div>
                     <div id="success"></div>
                     <form class="Form" id="contactForm" method="post">
-                        <div class="uk-padding-small"><input class="uk-input uk-width-1-2" id="fname" name="firstname" type="text" placeholder="First Name"></div>
-                        <div class="uk-padding-small"><input class="uk-input uk-width-1-2" id="lname" name="lastname" type="text" placeholder="Last Name"></div>
-                        <div class="uk-padding-small"><input class="uk-input uk-width-1-2" id="pnumber" name="phonenumber" type="text" placeholder="Phone Number"></div>
-                        <div class="uk-padding-small"><input class="uk-input uk-width-1-2" id="email" name="email" type="text" placeholder="email"></div>
-                        <div class="uk-padding-small"><textarea class="uk-textarea uk-width-1-1@s .uk-form-width-large" rows="7" id="message" name="message" placeholder="Message"></textarea></div>
+                        <div class="uk-padding-small"><p>Enter a first name:</p><input class="uk-input uk-width-1-2" id="fname" name="firstname" type="text" placeholder="First Name"></div>
+                        <div class="uk-padding-small"><p>Enter a last name:</p><input class="uk-input uk-width-1-2" id="lname" name="lastname" type="text" placeholder="Last Name"></div>
+                        <div class="uk-padding-small"><p>Enter a phone number:</p><input class="uk-input uk-width-1-2" id="pnumber" name="phonenumber" type="text" placeholder="Phone Number"></div>
+                        <div class="uk-padding-small"><p>Enter an email:</p><input class="uk-input uk-width-1-2" id="email" name="email" type="text" placeholder="email"></div>
+                        <div class="uk-padding-small"><p>Enter a message:</p><textarea class="uk-textarea uk-width-1-1@s .uk-form-width-large" rows="7" id="message" name="message" placeholder="Message"></textarea></div>
                         <div><button class="uk-button uk-button-primary uk-button-small uk-margin-small-left" type="submit">Submit</button></div>
                     </form>
 
