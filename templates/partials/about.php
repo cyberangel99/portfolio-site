7d28af745ffbe859ea6215f92a8302a4863da88d
Mary Green <cyberangel9911@gmail.com>
2019-02-11 15:41:03 -0600

    updated sections

--- templates/partials/about.php
+++ templates/partials/about.php
@@ -1,15 +1,16 @@
 <section id="about" class="uk-section uk-padding-remove-top uk-padding-remove-bottom">
     <div uk-grid class="uk-text-center">
+        <div class="uk-width-1-2@m uk-padding-remove-left" uk-scrollspy="cls: uk-animation-slide-bottom-medium; repeat: true; delay:500">
+            <div class="uk-overflow-hidden">
+                <img class="uk-animation-kenburns" src="./assets/images/aboutme.jpeg" alt="aboutme image"/>
+            </div>
+        </div>
         <div class="uk-width-1-2@m" uk-scrollspy="cls: uk-animation-slide-top-medium; repeat: true; delay:500">
             <h2 class="uk-margin-remove intro">Who am I?</h2>
             <p class="uk-padding-small uk-margin-remove">I am a web developer and I have basic skills in HTML, CSS, SASS/LESS, Javascript, and JQuery. I have been self-teaching myself these skills along with studying in class. I am very ambitious to learn more basic skills and progress to advance coding skills. My goal is to work as a Front-End web developer and my long-term goal is to become a Full-Stack web developer.</p> 
             <p class="uk-margin-remove uk-padding-small">I am a cheerful laid-back gal who is originally from Northwest Ohio and lived in the Chicago suburbs for a year. I currently live in the Upper Kirby area in Houston, TX. I attended college at College of DuPage in Glen Ellyn, IL and I have a Web Client Developer Certificate.</p> 
             <p class="uk-margin-remove uk-padding-small">Overall I'm just a geeky girl, who loves travelling, fashion, coding, art, gaming, watching anime, and riding my bike. I also have a nice collection of games and collectables that I love to collect and play!</p>
         </div>
-        <div class="uk-width-1-2@m uk-padding-remove-left" uk-scrollspy="cls: uk-animation-slide-bottom-medium; repeat: true; delay:500">
-            <div class="uk-overflow-hidden">
-                <img class="uk-animation-kenburns" src="./assets/images/aboutme.jpeg" alt="aboutme image"/>
-            </div>
-        </div>
+        
     </div>
 </section>
--- templates/partials/portfolio.php
+++ templates/partials/portfolio.php
@@ -3,11 +3,11 @@
     <div class="header uk-padding"><h2 class="portfolio">My Portfolio</h2></div>
     <div uk-grid class="uk-padding">
         <div class="uk-width-1-3@m" uk-scrollspy="cls:uk-animation-fade; repeat:true; delay: 300"><a href="https://cyberangel99.github.io/Farmermarket/" target="_blank"><img src="./assets/images/fmh-pic1.png" alt="portfolio 1"/></a></div>
-        <div class="uk-width-1-3@m" uk-scrollspy="cls:uk-animation-fade; repeat:true; delay: 400"><img src="./assets/images/portfolio2.jpg" alt="portfolio 2"/></div>
+        <!--<div class="uk-width-1-3@m" uk-scrollspy="cls:uk-animation-fade; repeat:true; delay: 400"><img src="./assets/images/portfolio2.jpg" alt="portfolio 2"/></div>
         <div class="uk-width-1-3@m" uk-scrollspy="cls:uk-animation-fade; repeat:true; delay: 500"><img src="./assets/images/portfolio3.jpg" alt="portfolio 3"/></div>
         <div class="uk-width-1-3@m" uk-scrollspy="cls:uk-animation-fade; repeat:true; delay: 600"><img src="./assets/images/portfolio4.jpg" alt="portfolio 4"/></div>
         <div class="uk-width-1-3@m" uk-scrollspy="cls:uk-animation-fade; repeat:true; delay: 700"><img src="./assets/images/portfolio5.jpg" alt="portfolio 5"/></div>
-        <div class="uk-width-1-3@m" uk-scrollspy="cls:uk-animation-fade; repeat:true; delay: 800"><img src="./assets/images/portfolio6.jpg" alt="portfolio 6"/></div>
+        <div class="uk-width-1-3@m" uk-scrollspy="cls:uk-animation-fade; repeat:true; delay: 800"><img src="./assets/images/portfolio6.jpg" alt="portfolio 6"/></div>-->
     </div>
     <div class="portfolio uk-text-center">
     <div class="header"><h2>Code samples</h2></div>
