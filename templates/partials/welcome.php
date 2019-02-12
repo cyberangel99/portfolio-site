4ededd7cf8a7c758f4fddb1fb814f793612e775f
Mary Green <cyberangel9911@gmail.com>
2019-02-11 20:49:00 -0600

    updated portfolio

--- templates/partials/welcome.php
+++ templates/partials/welcome.php
@@ -3,8 +3,8 @@
         <div class="uk-width-1-2@m uk-padding uk-background-muted" uk-scrollspy="cls:uk-animation-slide-bottom-medium; repeat: true; delay:500">
             <img class="uk-responsive-width" src="./assets/images/welcome.png" alt="welcome image"/>
         </div>        
-        <div class="uk-width-1-2@m text1" uk-scrollspy="cls: uk-animation-slide-top-medium; repeat: true; delay:500"><h2>Welcome to my site!</h2><p>Hi! My name is Mary and thanks for visiting my site! Why don't you stay for awhile? ;) You'll learn that I have a love for developing websites. I currently use Uikit frameworks but I also dabbled in bootstrap as well. I love the clean, single page, modern design of web pages nowadays.</p> 
-            <p>I am working on projects for my portfolio, please review portfolio and my Github for my source code.</p>
+        <div class="uk-width-1-2@m text_1" uk-scrollspy="cls: uk-animation-slide-top-medium; repeat: true; delay:500"><h2>Welcome to my site!</h2><p>Hi! My name is Mary and thanks for visiting my site! Why don't you stay for awhile? ;) You'll learn that I have a love for developing websites. I currently use Uikit frameworks but I also dabbled in bootstrap as well. I love the clean, single page, modern design of web pages nowadays.</p> 
+            <p>I am currently working on projects for my portfolio, please review my portfolio and my Github for my source code.</p>
         </div>
     </div>
 </section>
