96931a99a009f04133948eff7b0f4aa228745b49
Mary Green <cyberangel9911@gmail.com>
2019-02-17 17:07:40 -0600

    updated alignment

--- templates/partials/contact.php
+++ templates/partials/contact.php
@@ -4,16 +4,13 @@
             uk-scrollspy="cls: uk-animation-slide-bottom-medium; repeat: true; delay:500"><img
                 src="./assets/images/contactme.png" alt="contact me" />
         </div>
-        <div class="uk-width-1-2@m uk-padding" uk-scrollspy="cls: uk-animation-slide-top-medium; repeat: true; delay:500">
-            <div class="uk-padding uk-text-center">
+        <div class="uk-width-1-2@m text_2 uk-text-center" uk-scrollspy="cls: uk-animation-slide-top-medium; repeat: true; delay:500">
                 <h2 class="uk-margin-remove">Contact me</h2>
                 <p>If you are interested in my portfolio and have a position available, contact me anytime. I am
                     interested in a full-time Front End Web Developer position.</p>
                 <a class="email" href="mailto:magreen451@gmail.com">Email me</a>
                 <p>You can also call me at 419-789-1992, I am available anytime.</p>
                 <script type="text/javascript" src="./javascript/jquery-3.3.1.min.js"></script>
-            </div>
-
         </div>
 
     </div>
--- templates/partials/welcome.php
+++ templates/partials/welcome.php
@@ -1,13 +1,27 @@
 <section id="welcome" class="uk-section uk-padding-remove">
+    <!--mobile view-->
     <div uk-grid class="uk-text-center">
-        <div class="uk-width-1-2@m text_1 uk-hidden@m" uk-scrollspy="cls: uk-animation-slide-top-medium; repeat: true; delay:500"><h2>Welcome to my site!</h2><p>Hi! My name is Mary and thanks for visiting my site! Why don't you stay for awhile? ;) You'll learn that I have a love for developing websites. I currently use Uikit frameworks but I also dabbled in bootstrap as well. I love the clean, single page, modern design of web pages nowadays.</p> 
-            <p>I am currently working on projects for my portfolio, please review my portfolio and my Github for my source code.</p>
+        <div class="uk-width-1-2@m uk-hidden@m" uk-scrollspy="cls: uk-animation-slide-top-medium; repeat: true; delay:500">
+            <div class="uk-padding uk-text-center uk-margin-remove">
+                <h2>Welcome to my site!</h2>
+                    <p>Hi! My name is Mary and thanks for visiting my site! Why don't you stay for awhile? ;) You'll learn that
+                        I have a love for developing websites. I currently use Uikit frameworks but I also dabbled in bootstrap
+                        as well. I love the clean, single page, modern design of web pages nowadays.</p>
+                    <p>I am currently working on projects for my portfolio, please review my portfolio and my Github for my
+                        source code.</p>
+            </div>
         </div>
+    <!--desktop view of welcome section-->
         <div class="uk-width-1-2@m uk-background-muted" uk-scrollspy="cls:uk-animation-slide-bottom-medium; repeat: true; delay:500">
-            <img class="uk-responsive-width" src="./assets/images/welcome.png" alt="welcome image"/>
-        </div>        
-        <div class="uk-width-1-2@m text_1 uk-visible@m" uk-scrollspy="cls: uk-animation-slide-top-medium; repeat: true; delay:500"><h2>Welcome to my site!</h2><p>Hi! My name is Mary and thanks for visiting my site! Why don't you stay for awhile? ;) You'll learn that I have a love for developing websites. I currently use Uikit frameworks but I also dabbled in bootstrap as well. I love the clean, single page, modern design of web pages nowadays.</p> 
-            <p>I am currently working on projects for my portfolio, please review my portfolio and my Github for my source code.</p>
+            <img class="uk-responsive-width" src="./assets/images/welcome.png" alt="welcome image" />
+        </div>
+        <div class="uk-width-1-2@m uk-visible@m text_1" uk-scrollspy="cls: uk-animation-slide-top-medium; repeat: true; delay:500">
+            <h2>Welcome to my site!</h2>
+            <p>Hi! My name is Mary and thanks for visiting my site! Why don't you stay for awhile? ;) You'll learn that
+                I have a love for developing websites. I currently use Uikit frameworks but I also dabbled in bootstrap
+                as well. I love the clean, single page, modern design of web pages nowadays.</p>
+            <p>I am currently working on projects for my portfolio, please review my portfolio and my Github for my
+                source code.</p>
         </div>
     </div>
 </section>
